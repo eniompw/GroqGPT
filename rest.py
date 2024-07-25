97ae9e3dd86da766b377d7cd2bae4937a730d9bc
eniompw <71254710+eniompw@users.noreply.github.com>
2024-07-25 22:36:21 +0000

    simplify rest.py

--- rest.py
+++ rest.py
@@ -1,29 +1,10 @@
 import requests
 import os
 
-# Define the URL for the POST request
 url = "https://api.groq.com/openai/v1/chat/completions"
+headers = {"Authorization": "Bearer " + str(os.environ.get("GROQ_API_KEY"))}
+query = "Which nummber is bigger: 9.11 or 9.9?"
+data = {"messages": [{"role": "user", "content": query}], "model": "llama-3.1-70b-versatile"}
 
-# Create a dictionary for headers
-headers = {
-    "Content-Type": "application/json",  # Adjust content type as needed (e.g., application/xml)
-    "Authorization": "Bearer " + str(os.environ.get("GROQ_API_KEY")),  # Your authorization token
-}
-
-q = input("Enter your question here: ")
-# Prepare the data to be sent (can be JSON, string, etc.)
-messages = [{"role": "user", "content": q}]
-data = {"messages": messages, "model": "llama3-70b-8192"}
-
-# Send the POST request with headers and data
 response = requests.post(url, headers=headers, json=data)
-
-# Check the response status code
-if response.status_code == 200:
-  # print("Success! Data posted.")
-  # Access the response data (if any)
-  response_data = response.json()
-  print(response_data["choices"][0]["message"]["content"])
-else:
-  print("Error! Response code:", response.status_code)
-  print(response.json())
+print(response.json()["choices"][0]["message"]["content"])
